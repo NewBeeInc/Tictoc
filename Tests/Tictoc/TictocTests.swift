cc4c23170c21c89293ae75c0a818e4fe43daff0e
Ke Yang <ofveravi@gmail.com>
2016-06-18 00:23:13 +0800

    use new global variables for test case

--- Tests/Tictoc/TictocTests.swift
+++ Tests/Tictoc/TictocTests.swift
@@ -5,9 +5,9 @@ class TictocTests: XCTestCase {
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
-		XCTAssertTrue(NOW >= TODAY.lowerBound)
-		XCTAssertTrue(NOW < TODAY.upperBound)
-		XCTAssertTrue(Double(NOW) <= MOMENT)
+		XCTAssertTrue(tNow >= tToday.lowerBound)
+		XCTAssertTrue(tNow < tToday.upperBound)
+		XCTAssertTrue(Double(tNow) <= tMoment)
     }
 
 
