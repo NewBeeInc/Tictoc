3da26c52080f740714159b6fd4165ad8171d8a3d
Ke Yang <ofveravi@gmail.com>
2016-06-17 16:31:21 +0800

    update test case

--- Tests/Tictoc/TictocTests.swift
+++ Tests/Tictoc/TictocTests.swift
@@ -5,10 +5,9 @@ class TictocTests: XCTestCase {
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
-        XCTAssertEqual(Tictoc().text, "Hello, World!")
-		XCTAssertTrue(Tictoc().now >= Tictoc().today.lowerBound)
-		XCTAssertTrue(Tictoc().now < Tictoc().today.upperBound)
-		XCTAssertTrue(Double(Tictoc().now) <= Tictoc().moment)
+		XCTAssertTrue(NOW >= TODAY.lowerBound)
+		XCTAssertTrue(NOW < TODAY.upperBound)
+		XCTAssertTrue(Double(NOW) <= MOMENT)
     }
 
 
