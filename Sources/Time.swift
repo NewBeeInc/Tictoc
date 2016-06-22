ec0647197e5e225c44d99905c9b5def81a2853da
Ke Yang <ofveravi@gmail.com>
2016-06-22 16:04:06 +0800

    added Regex and Pattern for date/time string

--- Sources/Time.swift
+++ Sources/Time.swift
@@ -145,6 +145,26 @@ public extension Tictoc {
 		return tNow - c * SEC_PER_HOUR
 	}
 
+	public func dayFromShortDate(_ d: String) -> Day? {
 
+		return nil
+	}
+
+
+}
+
+struct RegexPattern {
+	static let FullDate = "[0-2]{1}\\d{3}\\-((((0[13578]{1})|(1[02]{1}))\\-((0[1-9]{1})|([12]{1}\\d{1})|(3[01]{1})))|(((0[469]{1})|(11))\\-((0[1-9]{1})|([12]{1}\\d{1})|(30)))|(02\\-((0[1-9]{1})|([12]{1}\\d{1}))))\\s(([01]{1}\\d{1})|2[0-3]{1}):[0-5]{1}\\d{1}:[0-5]{1}\\d{1}"
+	static let ShortDate = "[0-2]{1}\\d{3}\\-((((0[13578]{1})|(1[02]{1}))\\-((0[1-9]{1})|([12]{1}\\d{1})|(3[01]{1})))|(((0[469]{1})|(11))\\-((0[1-9]{1})|([12]{1}\\d{1})|(30)))|(02\\-((0[1-9]{1})|([12]{1}\\d{1}))))"
+}
+
+struct Regex {
+	#if os(Linux)
+	static let FullDate = try! NSRegularExpression(pattern: RegexPattern.FullDate, options: .caseInsensitive)
+	static let ShortDate = try! NSRegularExpression(pattern: RegexPattern.ShortDate, options: .caseInsensitive)
+	#else
+	static let FullDate = try! RegularExpression(pattern: RegexPattern.FullDate, options: .caseInsensitive)
+	static let ShortDate = try! RegularExpression(pattern: RegexPattern.ShortDate, options: .caseInsensitive)
+	#endif
 }
 
