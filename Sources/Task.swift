824b012f937973f7ffc9aa0b9137ec77e4a0ef24
Ke Yang <ofveravi@gmail.com>
2016-06-27 11:53:54 +0800

    modified comments

--- Sources/Task.swift
+++ Sources/Task.swift
@@ -33,22 +33,22 @@ func ==(lhs: Task, rhs: Task) -> Bool {
 
 class CountDownTask: Task {
 
-	/// a number counting down from(起始计数)
+	/// a number counting down from
 	private var from: Int?
 
-	/// a number count down to, default is 0(终止计数, 默认为0)
+	/// a number count down to, default is 0
 	private var to: Int = 0
 
-	/// count down interval in second, default is 1.0(计数间隔, 单位秒, 默认为1.0)
+	/// count down interval in second, default is 1.0
 	private var interval: Double = 1.0
 
-	/// a block called in progress of counting down(倒计时过程中的回调)
+	/// a block called in progress of counting down
 	private var at: ((Int) -> Void)?
 
-	/// a block called after counting down finished(倒计时结束时的回调)
+	/// a block called after counting down finished
 	private var finish: (() -> Void)?
 
-	/// a concurrent queue on which the task is attached(计时任务执行的队列)
+	/// a concurrent queue on which the task is attached
 	private var queue: DispatchQueue?
 
 	/// delegate
@@ -65,14 +65,14 @@ class CountDownTask: Task {
 
 	/**
 	designed initializer
-	(快速构造方法)
-	- parameter identifier: task identifier(任务ID)
-	- parameter from:       a number counting down from(起始计数)
-	- parameter to:         a number count down to, default is 0(终止计数, 默认为0)
-	- parameter interval:   count down interval in second, default is 1.0(计数间隔, 单位秒, 默认为1.0)
-	- parameter at:         a block called in progress of counting down(倒计时过程中的回调)
-	- parameter finish:     a block called after counting down finished(倒计时结束时的回调)
-	- returns: a CountDownTask instance(返回一个倒数计时任务实例)
+
+	- parameter identifier: task identifier
+	- parameter from:       a number counting down from
+	- parameter to:         a number count down to, default is 0
+	- parameter interval:   count down interval in second, default is 1.0
+	- parameter at:         a block called in progress of counting down
+	- parameter finish:     a block called after counting down finished
+	- returns: a CountDownTask instance
 	*/
 	convenience init(identifier: String, from: Int, to: Int = 0, interval: Double = 1.0, at: ((at: Int) -> Void)?, finish: (() -> Void)?) {
 		self.init()
@@ -90,8 +90,8 @@ internal extension CountDownTask {
 
 	/**
 	start counting down
-	(开始倒数计时)
-	- parameter from:  a number count down from(起始计数)
+
+	- parameter from:  a number count down from
 	*/
 	internal func startCountDown(from: Int) {
 		self.queue!.async { [weak self] in
@@ -101,8 +101,8 @@ internal extension CountDownTask {
 
 	/**
 	countind at
-	(倒数计时至)
-	- parameter at: a number counting down at(倒数至某个数)
+
+	- parameter at: a number counting down at
 	*/
 	internal func countDown(at: Int) {
 		print("countTo = \(at)")
@@ -122,14 +122,11 @@ internal extension CountDownTask {
 
 	/**
 	counting down finished
-	(结束倒数计时)
 	*/
 	internal func stopCountDown() {
 		DispatchQueue.main.async {
 			self.finish?()
-//			if self.delegate.responds(to: Selector(("taskWithDidFinish:"))) == true {
-//				self.delegate.task(didFinish: self)
-//			}
+			self.delegate?.task(didFinish: self)
 		}
 	}
 
